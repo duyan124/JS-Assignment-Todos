b4afc833678f78eaf373bdc62562b8503365f50b
duyan124 <binhlanduyan@gmail.com>
2021-03-06 18:01:42 +0700

    update item - fix filter

--- todoFunction.js
+++ todoFunction.js
@@ -1,9 +1,9 @@
 var items = [];
 var id_count = 0;
-
+var state = 'all';
 function addItem(event) {
   if (event.keyCode == 13) {
-    var txt = document.getElementById("txt_todo").value;
+    var txt = document.getElementById("txt_todo").value.trim();
     id = id_count;
     items.push({ txt, id, status: false });
 
@@ -19,18 +19,25 @@ function addItem(event) {
     newCheckBox.className = "checkbox";
     newCheckBox.id = `id-${id}`;
 
-    newPar = document.createElement("p");
+    newPar = document.createElement("input");
     newPar.id = `txt-${id}`;
-    var newContent = document.createTextNode(txt);
+    newPar.value = txt;
+    newPar.disabled = true;
+    newPar.addEventListener("blur", hideInput);
+
+    var newDblBtn = document.createElement("button");
+    newDblBtn.className = "btn__text";
+    newDblBtn.id = `btn-${id}`;
+    newDblBtn.addEventListener("dblclick", showInput);
 
     var newBtn = document.createElement("button");
     newBtn.className = "btn__del";
     newBtn.textContent = "x";
     newBtn.id = `${id}`;
 
-    newPar.appendChild(newContent);
+    newDblBtn.appendChild(newPar);
     newSubDiv.appendChild(newCheckBox);
-    newSubDiv.appendChild(newPar);
+    newSubDiv.appendChild(newDblBtn);
     newLi.appendChild(newSubDiv);
     newLi.appendChild(newBtn);
 
@@ -41,53 +48,66 @@ function addItem(event) {
 
     var ul = document.getElementById("container__list");
     id_count++;
-    newBtn.addEventListener("click", function () {
-      this.parentNode.remove();
-      const id = this.id;
-      items = items.filter((e) => e.id != id);
-      document.getElementById("items__left").textContent = `${
-        items.filter((e) => e.status == false).length
-      } `;
-    });
+    newBtn.addEventListener("click", delOneItem);
 
-    newCheckBox.addEventListener("click", function () {
-      let idCheck = this.id;
-      let id = idCheck.split("-")[1];
-      if (this.checked) {
-        for (let item of items) {
-          if (item.id == +id) item.status = true;
-        }
-         document.getElementById(`txt-${id}`).classList.add("input__completed");
-      } else {
-        for (let item of items) {
-          if (item.id == +id) item.status = false;
-        }
-        document
-          .getElementById(`txt-${id}`)
-          .classList.remove("input__completed");
-      }
-      document.getElementById("items__left").textContent = `${
-        items.filter((e) => e.status == false).length} `;
-      if (items.filter((e) => e.status == true).length == id_count)
-        document.getElementById("allOption").style.color = "gray";
-      else document.getElementById("allOption").style.color = null;
-    });
+    newCheckBox.addEventListener("click", checkCompleted);
 
     ul.appendChild(newLi);
   }
 }
+function showInput() {
+  let id = this.id.split("-")[1];
+  let ip = document.getElementById(`txt-${id}`);
+  ip.disabled = false;
+}
 
-function delAll() {
-    let arr = [];
+function hideInput() {
+  this.disabled = true;
+}
+
+function checkCompleted() {
+  let idCheck = this.id;
+  let id = idCheck.split("-")[1];
+  if (this.checked) {
     for (let item of items) {
-        if (item.status == true) {
-          let li = document.getElementById(`li-${item.id}`);
-          li.parentNode.removeChild(li);
-        } else {
-          arr.push(item);
-        }
-      }
-      items = arr;
+      if (item.id == +id) item.status = true;
+    }
+    document.getElementById(`txt-${id}`).classList.add("input__completed");
+  } else {
+    for (let item of items) {
+      if (item.id == +id) item.status = false;
+    }
+    document.getElementById(`txt-${id}`).classList.remove("input__completed");
+  }
+  document.getElementById("items__left").textContent = `${
+    items.filter((e) => e.status == false).length
+  } `;
+  if (items.filter((e) => e.status == true).length == id_count)
+    document.getElementById("allOption").style.color = "gray";
+  else document.getElementById("allOption").style.color = null;
+
+  filterOption(state);
+}
+
+function delOneItem() {
+  this.parentNode.remove();
+  const id = this.id;
+  items = items.filter((e) => e.id != id);
+  document.getElementById("items__left").textContent = `${
+    items.filter((e) => e.status == false).length
+  } `;
+}
+function delAll() {
+  let arr = [];
+  for (let item of items) {
+    if (item.status == true) {
+      let li = document.getElementById(`li-${item.id}`);
+      li.parentNode.removeChild(li);
+    } else {
+      arr.push(item);
+    }
+  }
+  items = arr;
 }
 
 function showAll() {
@@ -115,6 +135,18 @@ function completed() {
     } else {
       li.style.display = null;
     }
+  }
+}
+function filterOption(status) {
+  state = status;
+  if (status == "all") {
+    showAll();
+  }
+  if (status == "active") {
+    active();
+  }
+  if (status == "completed") {
+    completed();
   }
 }
 function allOption() {
@@ -145,4 +177,5 @@ function allOption() {
       items.filter((e) => e.status == false).length
     } `;
   }
+  filterOption(state);
 }
